98444ae9e93b3bd77be3d2183c2d78e9c1f1e939
Usil Gang <usilgang@gmail.com>
2022-01-10 14:05:12 +0500

    Update user_info_case.js

--- user_info_case.js
+++ user_info_case.js
@@ -119,7 +119,7 @@ $(function() {
      * Результат в коде Омнидеска https://www.dropbox.com/s/ilkkfmf81s63zjw/07_snow.png?dl=0
      * Результат на странице: https://www.dropbox.com/s/yp36vle7yu7vm38/07_2_snow.png?dl=0
      */
-    //$.get('https://cdnjs.cloudflare.com/ajax/libs/JQuery-Snowfall/1.7.4/snowfall.jquery.min.js', {
+    /*$.get('https://cdnjs.cloudflare.com/ajax/libs/JQuery-Snowfall/1.7.4/snowfall.jquery.min.js', {
 
     }, function(jsLibCode) {
         $("body").append($("<script />", {
@@ -127,6 +127,7 @@ $(function() {
         }));
     });
      $('body').css('overflow-y', 'hidden');
+     */
     /**
      * Таким образом можно менять и добавлять любые кнопки, формы и код на странице обращения.
      */
